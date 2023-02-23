8e202b4c34fcfaaec436225ee1b09aea2352e709
anyuz <zhengannyu@gmail.com>
2023-02-23 13:13:57 +0800

    4捨5入

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-var btn = document.getElementById('submit')
-var result1 = document.getElementById('result-1')
-var result2 = document.getElementById('result-2')
-btn.addEventListener('click', function () {
-  var w = parseInt(document.getElementById('width').value);
-  var h = parseInt(document.getElementById('height').value);
-  var total = (Math.ceil(w / 80)) * (h + 30) * 5;
-  result1.innerHTML = 'NT$ ' + total * 1.1;
-  result2.innerHTML = 'NT$ ' + total * 1.1 * 0.9;
+var btn = document.getElementById("submit");
+var result1 = document.getElementById("result-1");
+var result2 = document.getElementById("result-2");
+btn.addEventListener("click", function () {
+  var w = parseInt(document.getElementById("width").value);
+  var h = parseInt(document.getElementById("height").value);
+  var total = Math.ceil(w / 80) * (h + 30) * 5;
+  result1.innerHTML = "NT$ " + Math.round(total * 1.1);
+  result2.innerHTML = "NT$ " + Math.round(total * 1.1 * 0.9);
 });
