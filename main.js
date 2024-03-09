b6b63c503d2fdc2c55c942e92bdfb99e606ab8e6
Anyu Zheng <86967465+zhengannyu@users.noreply.github.com>
2024-03-09 14:59:35 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-var btn = document.getElementById("submit");
-var result1 = document.getElementById("result-1");
-var result2 = document.getElementById("result-2");
+let btn = document.getElementById("submit");
+let result1 = document.getElementById("result-1");
+let result2 = document.getElementById("result-2");
 btn.addEventListener("click", function () {
-  var w = parseInt(document.getElementById("width").value);
-  var h = parseInt(document.getElementById("height").value);
-  var total = Math.ceil(w / 80) * (h + 30) * 5;
+  let w = parseInt(document.getElementById("width").value);
+  let h = parseInt(document.getElementById("height").value);
+  let total = Math.ceil(w / 80) * (h + 30) * 5;
   result1.innerHTML = "NT$ " + Math.round(total * 1.1);
   result2.innerHTML = "NT$ " + Math.round(total * 1.1 * 0.9);
 });
